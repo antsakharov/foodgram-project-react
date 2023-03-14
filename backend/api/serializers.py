d94a45b1b5f1c2a5e9c80351f44c38db4f59dcd8
Антон Сахаров <av.sakharov.888@gmail.com>
2023-03-15 01:31:22 +0300

    after review

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,7 +1,6 @@
 from djoser.serializers import UserCreateSerializer, UserSerializer
 from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers
-
 from recipes.models import (Favorite, Ingredient, Recipe, RecipeIngredient,
                             RecipeTag, ShoppingCart, Tag)
 from users.models import Subscription, User
