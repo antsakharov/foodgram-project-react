b66452a06ad1bec9ed4d2b3e002b2138f17cca32
Антон Сахаров <av.sakharov.888@gmail.com>
2023-03-15 03:00:15 +0300

    after review

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,9 +2,8 @@ from django.db.models import Sum
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
-from djoser.views import UserViewSet
 from rest_framework import status, viewsets
-from rest_framework.decorators import api_view, action
+from rest_framework.decorators import api_view
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
